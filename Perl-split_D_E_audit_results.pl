dabda4dfd354720539325bc63c1a34af6a8c6411
fergusoa <aferguso@nrcan.gc.ca>
2015-08-24 15:08:08 -0400

    This commit updates the split script similar to changes made to the parsing script.

--- Perl-split_D_E_audit_results.pl
+++ Perl-split_D_E_audit_results.pl
@@ -1,84 +1,136 @@
 #!/bin/perl
  
+ 
+# Source file for raw D-E dump. 
 my $file_1 = "evaluations_d_e.csv";
 
+
+# File to contain D records
 my $file_D = "split-results_D_audit.txt";
+
+# File to contain E records. 
 my $file_E = "split-results_E_audit.txt";
  
+ 
+# Oppen files. 
 open (FILEIN1, $file_1) or die ("could not open $file_1\n");
 
 open (FILEOUT_D, ">$file_D") or die ("could not open $file_D\n"); 
 open (FILEOUT_E, ">$file_E") or die ("could not open $file_E\n"); 
 
-
-$line_no = 0; 
-
+# Counters
+my $line_no        = 0; 
+my $line_no_buffer = 0; 
 my $eval_col_index = 0;
+
+# output 
+my $output   = ""; 
+my $output_D = ""; 
+my $output_E = ""; 
+
+# Loop through file, 
+#  1. Find the columns we want to keep, or optionally keep them all, 
+#  2. Determine if this is a D or E record,
+#  3. Store results in D or E output, 
+#  4. Periodically write D or E output to file. 
 
 while ( my $line = <FILEIN1> ){
 
-$line =~ s/([^\"]),([^\"])/$1_$2/g;
 
-#print $line ; 
-
-
-my $colindex = 0; 
-
-
-
+  # Escape unnecssary spaces. 
+  $line =~ s/([^\"]),([^\"])/$1_$2/g;
 
   
- 
+  my $colindex = 0; 
+
+  # Take the line and split it into columns
   my @columns = split /,/, $line ; 
-
-
+  
+  # On the first line, loop through the columns and get the ones we want 
+  # to keep. 
   if ($line_no == 0) {
   
     foreach my $column (@columns) {
      #print ">$column\n"; 
-	 $colindex++;
-     if ( $column =~ /HOUSE_ID/) { push @keep_col_index, $colindex-1; }
-	 if ( $column =~ /EVAL_TYP/) { push @keep_col_index, $colindex-1;
-        	$eval_col_index =  $colindex -1 ; }
-	 if ( $column =~ /PROVINCE/) { push @keep_col_index, $colindex-1; }	
-	 if ( $column =~ /DECADEBUILT/) { push @keep_col_index, $colindex-1; }
- 	 if ( $column =~ /FLOORAREA/) { push @keep_col_index, $colindex-1; }
-	 if ( $column =~ /FOOTPRINT/) { push @keep_col_index, $colindex-1; }
-	 if ( $column =~ /TYPEOFHOUSE/) { push @keep_col_index, $colindex-1; }	
-	 if ( $column =~ /STOREYS/) { push @keep_col_index, $colindex-1; }
-	 if ( $column =~ /\"FURSSEFF/) { push @keep_col_index, $colindex-1; }
-	 if ( $column =~ /\"FURNACEFUEL/) { push @keep_col_index, $colindex-1; }	 
-	 if ( $column =~ /\"PDHWEF/) { push @keep_col_index, $colindex-1; }	
-	 if ( $column =~ /\"PDHWFUEL/) { push @keep_col_index, $colindex-1; }	
-     if ( $column =~ /\"CEILINS/) { push @keep_col_index, $colindex-1; }	 
-	 if ( $column =~ /\"FNDWALLINS/) { push @keep_col_index, $colindex-1; }	
-	 if ( $column =~ /\"MAINWALLINS/) { push @keep_col_index, $colindex-1; }	
- 	 if ( $column =~ /\"WINDOWCODE/) { push @keep_col_index, $colindex-1; }	
-     if ( $column =~ /\"AIR50P/) { push @keep_col_index, $colindex-1; }  
-     if ( $column =~ /EINCENTIVE/) { push @keep_col_index, $colindex-1; }  	 	  
+	   $colindex++;
+	 
+     # Store index (Can be tested to produce a subset of the 
+     #              current column list )
+	   push @keep_col_index, $colindex-1;
+	      
+        
+     # Also sort the location of the 'eval_type' column - we need this to
+     # figure out if it's a d or e audit file
+     if ( $column =~ /EVAL_TYP/) {	$eval_col_index =  $colindex -1 ;  }
+   
+     #if ( $column =~ /HOUSE_ID/) { push @keep_col_index, $colindex-1; }
+	   # { push @keep_col_index, $colindex-1;
+     #   	$eval_col_index =  $colindex -1 ; }
+	   #if ( $column =~ /PROVINCE/) { push @keep_col_index, $colindex-1; }	
+	   #if ( $column =~ /DECADEBUILT/) { push @keep_col_index, $colindex-1; }
+ 	   #if ( $column =~ /FLOORAREA/) { push @keep_col_index, $colindex-1; }
+	   #if ( $column =~ /FOOTPRINT/) { push @keep_col_index, $colindex-1; }
+	   #if ( $column =~ /TYPEOFHOUSE/) { push @keep_col_index, $colindex-1; }	
+	   #if ( $column =~ /STOREYS/) { push @keep_col_index, $colindex-1; }
+	   #if ( $column =~ /\"FURSSEFF/) { push @keep_col_index, $colindex-1; }
+	   #if ( $column =~ /\"FURNACEFUEL/) { push @keep_col_index, $colindex-1; }	 
+	   #if ( $column =~ /\"PDHWEF/) { push @keep_col_index, $colindex-1; }	
+  	 #if ( $column =~ /\"PDHWFUEL/) { push @keep_col_index, $colindex-1; }	
+     #if ( $column =~ /\"CEILINS/) { push @keep_col_index, $colindex-1; }	 
+  	 #if ( $column =~ /\"FNDWALLINS/) { push @keep_col_index, $colindex-1; }	
+	   #if ( $column =~ /\"MAINWALLINS/) { push @keep_col_index, $colindex-1; }	
+ 	   #if ( $column =~ /\"WINDOWCODE/) { push @keep_col_index, $colindex-1; }	
+     #if ( $column =~ /\"AIR50P/) { push @keep_col_index, $colindex-1; }  
+     #if ( $column =~ /EINCENTIVE/) { push @keep_col_index, $colindex-1; }  	 	  
 
 
     }
 
   }
 
+  if ( $line_no_buffer > 4999 ){
+     
+     print "Dumping $line_no_buffer lines ($line_no) \n"; 
+     
+     
+     print FILEOUT_D "$output_D";
+     print FILEOUT_E "$output_E";
+     
+     
+     
+     $output_D = ""; 
+     $output_E = ""; 
+     
+     $line_no_buffer = 0; 
+     
+  }
   
   
   $output = ""; 
+  # Append good columns to a temp string. 
   foreach my $column (@keep_col_index ){
-    print "LINE: $line_no COL:  $column \n"; 
-	$output .= $columns[$column].",";
-	
-	 
+    #print "LINE: $line_no COL:  $column \n"; 
+	  $output .= $columns[$column].",";
   }
-  print ">>> $eval_col_index $columns[$eval_col_index] \n";  
-  if ( $columns[$eval_col_index] =~ /EVAL_TYP/ ) {  print FILEOUT_D "$output\n";
-                                                    print FILEOUT_E "$output\n";  }
-  if ( $columns[$eval_col_index] =~ /D/ ) {  print FILEOUT_D "$output\n"; }
-  if ( $columns[$eval_col_index] =~ /\"E"/ ) {  print FILEOUT_E "$output\n"; }
+  $output .= "\n"; 
+  # determine if this is a D or E string. 
 
 
-$line_no++; 
+  #print ">>> $line_no $eval_col_index  $columns[0]  $columns[$eval_col_index] \n";  
+  
+  # Hader row: 
+  if ( $columns[$eval_col_index] =~ /EVAL_TYP/ ) {  $output_D .= $output; $output_E .= $output;  }                                  
+  if ( $columns[$eval_col_index] =~ /D/ )        {  $output_D .= $output; }
+  if ( $columns[$eval_col_index] =~ /\"E"/ )     {  $output_E .= $output; }
+  
+  # Empty column header. 
+  @columns = (); 
+
+  
+  # Increment counter. 
+  $line_no++; 
+  $line_no_buffer++; 
+  
 }
 
 foreach my $column ( @keep_col_index ){
--- readme.txt
+++ readme.txt
@@ -1,11 +1,20 @@
 This repository contains 3 Perl scripts that manipulate the data in the ERS database.
 
 1. Perl-parse-script.pl
-This script parses the full EGH database for only those columns of interest for the particular project.
-It reads in the full EGH database "evaluations_d_e.csv" and writes the parsed data to a text file.
+
+* THIS SCRIPT's Functionality has been supplanted by split_D_E_audit_results.pl *
+
+This script parses the full EGH database for only those columns of interest for the 
+particular project. It reads in the full EGH database "evaluations_d_e.csv" 
+and writes the parsed data to a text file.
+
+Usage: 
+
+   Perl-parse-script.pl <input csv file> 
 
 2. Perl-split_D_E_audit_results.pl
-This script reads in the output file from Perl-parse-script.pl and writes out two text files.
+This script reads in the output file from Perl-parse-script.pl and writes 
+out two text files.
 	"split-results_D_audit.txt" contains only D-audit data
     "split-results_E_audit.txt" contains only E-audit data
 
